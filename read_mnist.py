f85e70312bd89df8948d430518a9f840275dbecb
Daichi Fukuoka <daichi.fukuoka@hpe.com>
2017-10-13 08:25:16 +0900

    update read_mnist.py

--- read_mnist.py
+++ read_mnist.py
@@ -38,7 +38,7 @@ def read_mnist():
 
     return {"labels":labels, "images":images}
 
-if __name__ == "__main__":
+def main():
     argv = sys.argv
     argc = len(argv)
     if argc == 1:
@@ -53,3 +53,6 @@ if __name__ == "__main__":
         
     print("answer: ", train_data[labels][img_no])
     draw_heatmap(train_data[images][img_no,:,:], 0, 255)
+    
+if __name__ == "__main__":
+    main()
